d84a6f3d9f687c9193fd71565026f6722514e9ea
Quero Competir <deploy@querocompetir.com.br>
2026-08-18 12:14:11 -0300

    chore: remove test files

--- register.ts
+++
@@ -1,8 +0,0 @@
-const phone = "5571991414913";
-const formattedTo = (function formatPhoneBR(phone) {
-  let clean = (phone || "").replace(/\D/g, "");
-  if (clean.length >= 10 && !clean.startsWith("55")) clean = "55" + clean;
-  return clean;
-})(phone);
-
-console.log("Formatted:", formattedTo, "Length:", formattedTo.length);
